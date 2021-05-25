78500ec35e76e3385c5388a07d6e46a501e45255
steffkes <stefan@gelungen.es>
2021-05-25 17:10:17 +0200

    fetch spiegel & bild as well

--- reizwortgenerator/spiders/rss.py
+++ reizwortgenerator/spiders/rss.py
@@ -12,6 +12,11 @@ class RssSpider(scrapy.Spider):
             ("AFD", "https://afdkompakt.de/feed/"),
             ("LINKE", "https://www.die-linke.de/start/presse/feed.rss"),
             ("GRUENE", "https://www.gruene.de/feeds/neues.html"),
+            ("SPIEGEL_PANORAMA", "https://www.spiegel.de/panorama/index.rss"),
+            (
+                "BILD_LIFESTYLE",
+                "https://www.bild.de/rssfeeds/vw-lifestyle/vw-lifestyle-16728898,dzbildplus=true,short=1,sort=1,teaserbildmobil=false,view=rss2.bild.xml",
+            ),
         ]:
             yield scrapy.http.Request(url, cb_kwargs={"key": key})
 
